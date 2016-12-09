4815065dd1fc5eac509528e01ead26bbabff1516
Margie Nevarez <margie.ynm@gmail.com>
2016-12-08 20:47:04 -0800

    add all / equal method to stylist class to pass two specs

--- lib/stylist.rb
+++ lib/stylist.rb
@@ -6,4 +6,13 @@ attr_reader(:name, :id)
     @name = attributes.fetch(:name)
     @id = attributes.fetch(:id, nil)
   end
+
+  define_singleton_method(:all) do
+    DB.exec("SELECT * FROM stylists").map do |stylist|
+      Stylist.new({:name => ('name'), :id => ('id').to_i})
+    end
+  end
+  define_method(:==) do |other|
+    self.name().==(other.name) && self.id.==(other.id)
+  end
 end
--- spec/stylist_spec.rb
+++ spec/stylist_spec.rb
@@ -1,11 +1,25 @@
 require('spec_helper.rb')
 
 describe(Stylist) do
+  before(:each) do
+  @stylist = Stylist.new(:name => 'Gwen')
+  end
 
   describe('#initialize') do
     it('adds attributes to class') do
-      @stylist = Stylist.new(:name => 'Gwen')
+      # @stylist = Stylist.new(:name => 'Gwen')
       expect(@stylist.name).to eq('Gwen')
+    end
+  end
+  describe('.all') do
+    it('will contain no stylists to begin with') do
+      expect(Stylist.all()).to eq([])
+    end
+  end
+  describe('.==') do
+    it('two instances with the same name and id will be equal') do
+      stylist = Stylist.new(:name => 'Gwen')
+      expect(@stylist).to eq(stylist)
     end
   end
 end
