20f1894b39ea6004c9325d2a28e3daa3c689083b
Margie Nevarez <margie.ynm@gmail.com>
2016-12-08 20:10:18 -0800

    will add initial files and requirements

--- spec/stylist_spec.rb
+++ spec/stylist_spec.rb
@@ -2,13 +2,12 @@ require('spec_helper')
 
 describe(Stylist) do
   before(:each) do
-
   end
-end
 
-describe('#initialize') do
-  it('adds attributes to class') do
-    @stylist = Style.new(:name => 'Gwen')
-    expect(@stylist.name)to. eq('Gwen')
+  describe('#initialize') do
+    it('adds attributes to class') do
+      @stylist = Style.new(:name => 'Gwen')
+      expect(@stylist.name).to eq('Gwen')
+    end
   end
 end
